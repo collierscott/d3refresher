4f7f2b3e9afefb68abe19e7d0300099caba8aec0
Scott Collier <=>
2019-07-16 15:22:42 +0200

    Update data and adjust domain

--- interactive/app.js
+++ interactive/app.js
@@ -56,7 +56,9 @@ svg.selectAll( 'text' )
 
 // Events
 d3.select('button').on('click', function() {
-    data.reverse();
+    // data.reverse();
+    data[0] = 50;
+    y_scale.domain([0, d3.max(data)]);
     svg.selectAll('rect')
         .data(data)
         .transition()
